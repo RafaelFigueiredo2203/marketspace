c9ece7aaf1d931ada216bf823c96dfbc62f56564
Rafa Figueiredo <rafaelfigueiredojunior7@gmail.com>
2024-10-24 07:20:41 -0300

    iniciando modal

--- src/components/Marketplace/Marketplace.tsx
+++ src/components/Marketplace/Marketplace.tsx
@@ -1,9 +1,25 @@
-import { Faders, MagnifyingGlass } from 'phosphor-react-native'
-import React from 'react'
-import { Text, TextInput, TouchableOpacity, View } from 'react-native'
+import Checkbox from 'expo-checkbox'
+import { Faders, MagnifyingGlass, X } from 'phosphor-react-native'
+import React, { useState } from 'react'
+import {
+  Alert,
+  Modal,
+  Pressable,
+  ScrollView,
+  Switch,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native'
 import { ProductCard } from './Product/ProductCard'
 
 export function Marketplace() {
+  const [modalVisible, setModalVisible] = useState(false)
+  const [isEnabled, setIsEnabled] = useState(false)
+  const toggleSwitch = () => setIsEnabled((previousState) => !previousState)
+  const [isChecked, setChecked] = useState(false)
+
   return (
     <View>
       <Text className="mt-8 text-left text-sm font-light text-gray-3">
@@ -22,16 +38,86 @@ export function Marketplace() {
             <MagnifyingGlass weight="bold" size={24} />
           </TouchableOpacity>
           <View className="h-6 w-[2px] bg-slate-400" />
-          <TouchableOpacity>
+          <TouchableOpacity onPress={() => setModalVisible(true)}>
             <Faders weight="bold" size={24} />
           </TouchableOpacity>
         </View>
       </View>
 
-      <View className="w-full border mt-10  flex  flex-wrap flex-row ">
-        <ProductCard />
-        <ProductCard />
-      </View>
+      <ScrollView showsVerticalScrollIndicator={false} className="mt-10 mb-80 ">
+        <View className="w-full   flex  flex-wrap flex-row ">
+          <ProductCard />
+        </View>
+      </ScrollView>
+
+      <Modal
+        animationType="fade"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert('Modal has been closed.')
+          setModalVisible(!modalVisible)
+        }}
+      >
+        <View className="bg-gray-900/80 h-screen flex items-end justify-end">
+          <View className="h-2/3 w-full flex items-center bg-bg-gray_6 rounded-t-3xl px-6 pt-4">
+            <View className="bg-gray-400 h-1 w-14 rounded-lg" />
+
+            <View className="flex flex-row w-full items-center justify-between mt-7">
+              <Text className="text-xl font-bold text-gray-950">
+                Filtrar anúncios
+              </Text>
+              <Pressable onPress={() => setModalVisible(!modalVisible)}>
+                <X size={24} />
+              </Pressable>
+            </View>
+
+            <View className="flex w-full flex-col items-left mt-6">
+              <Text className="text-sm font-bold text-gray-2">Condição</Text>
+
+              <View className="flex w-full flex-row mt-4">
+                <TouchableOpacity className="flex items-center justify-center w-[76px] h-7 rounded-2xl bg-bg-purple mr-4">
+                  <Text className="uppercase text-xs text-white font-bold">
+                    Novo
+                  </Text>
+                </TouchableOpacity>
+                <TouchableOpacity className="flex items-center justify-center w-[76px] h-7 rounded-2xl bg-gray-300">
+                  <Text className="uppercase text-xs text-gray-700 font-bold">
+                    Usado
+                  </Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+
+            <View className="flex w-full flex-col items-left mt-6">
+              <Text className="text-sm font-bold text-gray-2">
+                Aceita troca?
+              </Text>
+
+              <Switch
+                className="mt-4"
+                trackColor={{ false: ' #374151', true: '#647ac7' }}
+                thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
+                ios_backgroundColor=" #374151"
+                onValueChange={toggleSwitch}
+                value={isEnabled}
+              />
+            </View>
+
+            <View className="flex w-full flex-col items-left mt-6">
+              <Text className="text-sm font-bold text-gray-2">
+                Meios de pagamento aceitos
+              </Text>
+
+              <Checkbox
+                value={isChecked}
+                onValueChange={setChecked}
+                color={isChecked ? '#4630EB' : undefined}
+              />
+            </View>
+          </View>
+        </View>
+      </Modal>
     </View>
   )
 }
--- src/components/Marketplace/Product/ProductCard.tsx
+++ src/components/Marketplace/Product/ProductCard.tsx
@@ -1,18 +1,32 @@
 import React from 'react'
-import { ImageBackground, Text, View } from 'react-native'
+import {
+  Image,
+  ImageBackground,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native'
 import productImage from '../../../assets/product.png'
 
 export function ProductCard() {
   return (
     <>
-      <View className="flex flex-col items-center justify-center w-1/2 p-2">
+      <TouchableOpacity className="flex flex-col items-center justify-center w-1/2 p-2 mb-5">
         <ImageBackground
           source={productImage}
           resizeMode="cover"
           borderRadius={10}
           className="w-[153px] h-[100px] flex flex-row justify-between px-2 pt-1"
         >
-          <Text>Foto</Text>
+          <View className="rounded-full border-[2px] border-[#e2e3e7] flex items-center justify-center h-7">
+            <Image
+              alt="image-user"
+              className="rounded-full"
+              source={require('../../../assets/profile.jpeg')}
+              style={{ width: 23, height: 23 }}
+            />
+          </View>
+
           <View className="w-14 h-4 flex items-center justify-center rounded-lg bg-slate-700">
             <Text className="text-[10pt] font-bold text-center text-white">
               USADO
@@ -20,9 +34,13 @@ export function ProductCard() {
           </View>
         </ImageBackground>
 
-        <Text>Tênis vermelho</Text>
-        <Text>R$ 59,90</Text>
-      </View>
+        <View className="flex w-full flex-col items-left mt-2">
+          <Text className="text-sm text-gray-2">Tênis vermelho</Text>
+          <Text className="text-base text-gray-950 font-bold">
+            <Text className="text-xs">R$</Text> 59,90
+          </Text>
+        </View>
+      </TouchableOpacity>
     </>
   )
 }
